4bf828b540eedc866946f804118933e68d8095f9
samerayari <samerayari2807@yahoo.dk>
2025-11-17 13:31:55 +0100

    Update Program.cs

--- aflevering2/Program.cs
+++ aflevering2/Program.cs
@@ -1,15 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 Console.WriteLine("How many motors are carrying the packages?");
 var antalmotor= Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("How many kg of packages do we expect?");
 var antalkg = Convert.ToDouble(Console.ReadLine());
 
-if(antalkg/antalmotor>=5.6) 
-    Console.WriteLine("Yes");
+if(antalkg/antalmotor<=5.6) 
+    Console.WriteLine("Yes!");
 else
 {
     Console.WriteLine("No");
 }
-
